0649abb3543ea1c296d76924900aba4c36e46e85
niceqi110164 <30921833+niceqi110164@users.noreply.github.com>
2018-11-30 21:26:01 +0800

    Update Life.vue

--- src/components/Life.vue
+++ src/components/Life.vue
@@ -2,7 +2,7 @@
     <div id="life">
         <h2>这是life 组件 ---- {{msg}}</h2>
         <br>
-        <button @click="getMsg()">点击改变数据11111111111</button>
+        <button @click="getMsg()">点击改变数据111111111112222222</button>
     </div>
 </template>
 
